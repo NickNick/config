c82631137d20306ba970f026b9cef625d5e7efac
Nick Overdijk <nick@astrant.net>
2024-01-11 04:48:50 +0100

    upd

--- fish/functions/hx.fish
+++ fish/functions/hx.fish
@@ -1,3 +1,10 @@
-function hx --wraps=helix --description 'alias hx helix'
-  helix $argv; 
+function hx
+  if command -q hx
+    # Use command again because otherwise we would execute this function.
+    command hx $argv;
+  else if command -q helix
+    command helix $argv;
+  else
+    echo "Neither hx nor helix found"
+  end
 end
