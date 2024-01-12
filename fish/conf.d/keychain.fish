a294847060af4af45786f6653d0e3b99ed837fef
Nick Overdijk <nick@astrant.net>
2024-01-12 09:39:01 +0100

    Fix keychain

--- fish/conf.d/keychain.fish
+++ fish/conf.d/keychain.fish
@@ -1,5 +1,3 @@
 if status is-interactive
-    # To add a key, set -Ua SSH_KEYS_TO_AUTOLOAD keypath
-    # To remove a key, set -U --erase  SSH_KEYS_TO_AUTOLOAD[index_of_key]
-    keychain --eval --quiet $SSH_KEYS_TO_AUTOLOAD | source
+    keychain --eval --quiet -Q id_rsa | source
 end
