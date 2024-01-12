507609c7845d69bb5acdf8be2379eae3c2420165
Nick Overdijk <nick@astrant.net>
2024-01-12 09:44:33 +0100

    upd

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+set -gx SHELL fish
+set -gx DFT_COLOR always
+set -gx INSTA_TEST_RUNNER nextest
 
+if status is-interactive
     starship init fish | source
     set -gx EDITOR hx
 end
