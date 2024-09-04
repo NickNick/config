fe66a8151f672f0d299a04c437e138007627d7df
Nick Overdijk <nick@astrant.net>
2024-09-04 11:03:30 +0200

    upd

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 set -gx SHELL fish
 set -gx DFT_COLOR always
 set -gx INSTA_TEST_RUNNER nextest
+set -gx EDITOR hx
 
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
-    set -gx EDITOR hx
+    fzf --fish | source
 end
