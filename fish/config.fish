c060f35949cf6039ba847360ff6dfbd958f56e4f
Nick Overdijk <nick@astrant.net>
2024-09-23 10:25:45 +0200

    upd

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,10 @@ set -gx DFT_COLOR always
 set -gx INSTA_TEST_RUNNER nextest
 set -gx EDITOR hx
 
+fish_add_path ~/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
